a11581dc35772d0ba23919be5ca396e443d1387b
Darsh-coder <75057254+Darsh-coder@users.noreply.github.com>
2021-06-26 20:13:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,44 +19,47 @@ function setup() {
   iss=createSprite(400, 200, 50, 50);
   rovertoched=createSprite(333, 226, 20, 20);
   rovertoched.visible=false
+ //rovertoched.debug=true
+ 
   spacecraft=createSprite(400, 350, 50, 50);
   hasDocked =false
   spacecraft.scale=0.2
   iss.addImage("iss",issimg);
-   spacecraft.addImage("spacer",spacecraft1img)
+  spacecraft.addImage("spacer",spacecraft1img)
+  //spacecraft.debug=true
+  spacecraft.setCollider("circle",0,0,200)
 }
 
 function draw() {
   background(spacebgimg);  
   drawSprites();
   
- 
   if(!hasDocked){
     this.x = Math.random(100,200)
+
   }
   if(spacecraft.isTouching(rovertoched)){
     textSize(40)
    text("docking sucessful",235,350)
-
+   spacecraft.velocityY=0
+   spacecraft.velocityX=0
 
 
   }
 
 if(keyIsDown(UP_ARROW)){
-  spacecraft.velocityX=1
+  spacecraft.y=spacecraft.y-2
 spacecraft.addImage("spacAerr",spacecraft2img);
 }
-if(keyIsDown(DOWN_ARROW)){
-  spacecraft.velocityX=-1
-spacecraft.addImage("spacDerr",spacecraft1img);
-}
 if(keyIsDown(LEFT_ARROW)){
-  spacecraft.velocityX=1
+  spacecraft.x=spacecraft.x-2
 spacecraft.addImage("sApacerr",spacecraft4img);
 }
 if(keyIsDown(RIGHT_ARROW)){
-  spacecraft.velocityX=-1
+  spacecraft.x=spacecraft.x+2
 spacecraft.addImage("spaScerr",spacecraft3img);
 }
 }
+
+
 
